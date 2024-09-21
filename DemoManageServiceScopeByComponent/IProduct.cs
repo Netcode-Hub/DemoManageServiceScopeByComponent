08c4dcada395ebdd3ab61e3ff1254095eb268c8a
Frederick Asante <Rastinga1995@hotmail.com>
2024-09-21 18:24:24 +0000

    Add project files.

---
+++ DemoManageServiceScopeByComponent/IProduct.cs
@@ -0,0 +1,29 @@
+﻿namespace DemoManageServiceScopeByComponent
+{
+    public interface IProduct :IDisposable
+    {
+        void AddProduct(Product product);
+        List<Product> GetProducts();
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+
+    public class ProductService : IProduct
+    {
+        public List<Product> Products { get; set; } = [];
+        public void AddProduct(Product product) => Products.Add(product);
+
+        public bool IsDispose = false;
+        public void Dispose()
+        {
+            IsDispose = true;
+        }
+
+        public List<Product> GetProducts() => Products;
+    }
+}
